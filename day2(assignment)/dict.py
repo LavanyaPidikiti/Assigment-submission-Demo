96122301c361b690f54fb75b4aeaa144bc1f0515
LavanyaPidikiti <74549428+LavanyaPidikiti@users.noreply.github.com>
2020-11-16 19:58:06 +0530

    My Day2- Assigments

---
+++ day2(assignment)/dict.py
@@ -0,0 +1,5 @@
+a={}
+n=8
+for i in range(1,n+1):
+   a.update({i:i*i})
+print(a,end="")
---
+++ day2(assignment)/distance.py
@@ -0,0 +1,12 @@
+#n=number of directions
+import math
+n=4
+up=5
+down=3
+left=3
+right=2
+p1=(x1,y1)=(0,0)
+p2=(x2,y2)=(1,2)
+d=math.sqrt((x2-x1)**2+(y2-y1)**2)
+a=int(d)
+print("distance","=",a)
