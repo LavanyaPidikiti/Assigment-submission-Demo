75718bf8ddee033fa776108779339d57e75f3450
LavanyaPidikiti <74549428+LavanyaPidikiti@users.noreply.github.com>
2020-11-16 19:55:24 +0530

    My Day-1 Assignments

---
+++ day1(assignment)/euro.py
@@ -0,0 +1,5 @@
+#s=euro dollars
+s=[20,50,72,7,35,23,95,18]
+c=len(s)
+for i in range(c):
+   print("test case",i+1,"input",s[i],"output",s[i]*80)
---
+++ day1(assignment)/price.py
@@ -0,0 +1,9 @@
+costprice=20
+sellingprice=30
+if costprice>sellingprice:
+    print("loss")
+elif costprice==sellingprice:
+    print("neither")
+else:
+    print("profit")
+    
